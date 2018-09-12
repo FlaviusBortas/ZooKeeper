6524db1e2dca94c513e87b4178f68bd6bc5da7e7
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-09-12 13:35:05 -0400

    Added animal class and main VC

---
+++ ZooKeeperApp/Animal.swift
@@ -0,0 +1,23 @@
+//
+//  Animal.swift
+//  ZooKeeperApp
+//
+//  Created by Flavius Bortas on 9/12/18.
+//  Copyright © 2018 Flavius Bortas. All rights reserved.
+//
+
+import Foundation
+
+class Animal {
+    var name: String
+    var species: String
+    var gender: String
+    
+    init(name: String, species: String, gender: String) {
+        self.name = name
+        self.species = species
+        self.gender = gender
+    }
+    
+    
+}
