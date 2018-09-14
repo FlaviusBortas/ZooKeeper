7d642ad5840181984f8dc053355ed5431c117162
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-09-14 10:48:59 -0400

    Added AddAnimal VC, NewAnimalDetailsVC along iwth conneting elements to correct VC's

--- ZooKeeperApp/NewAnimalDetailsViewController.swift
+++ ZooKeeperApp/NewAnimalDetailsViewController.swift
@@ -10,26 +10,40 @@ import UIKit
 
 class NewAnimalDetailsViewController: UIViewController {
 
+    //MARK: - UI Elelments
+    
+    @IBOutlet weak var animalName: UITextField!
+    @IBOutlet weak var animalSpecies: UITextField!
+    @IBOutlet weak var animalGender: UITextField!
+    @IBOutlet weak var animalAge: UITextField!
+    
+    
+    
+    //MARK: - Properties
+    
+    
+    
+    
+    //MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Actions
+    
+    @IBAction func doneTapped(_ sender: UIBarButtonItem) {
     }
-    */
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    
+    //MARK: - Methods
+    
+    
+    
 
 }
