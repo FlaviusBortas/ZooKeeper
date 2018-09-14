0bce045eb28dee2ac080ed5b14fa13eaae6f48d7
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-09-14 10:51:47 -0400

    nothing changed

--- ZooKeeperApp/HabitatsTableViewController.swift
+++ ZooKeeperApp/HabitatsTableViewController.swift
@@ -61,18 +61,4 @@ class HabitatsTableViewController: UITableViewController {
             return
         }
     }
-
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
 }
